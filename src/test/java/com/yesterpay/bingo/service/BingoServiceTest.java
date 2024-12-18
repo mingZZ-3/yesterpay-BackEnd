43e0a31b0037d4d0c04759c3168cc6b7ab54e0c1
KingMing <alswl7754@gmail.com>
2024-12-18 17:34:44 +0900

    Test : 잘못된 테스트 케이스 제거

--- src/test/java/com/yesterpay/bingo/service/BingoServiceTest.java
+++ src/test/java/com/yesterpay/bingo/service/BingoServiceTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -27,30 +29,9 @@ class BingoServiceTest {
     @DisplayName("글자 획득 시, 빙고 체크 테스트")
     void 글자_획득시_빙고_체크() {
         //given
-        BingoCheckByLetterListDTO bingoCheckByLetterListDTO1 = new BingoCheckByLetterListDTO();
-        bingoCheckByLetterListDTO1.setMemberId(1L);
-        bingoCheckByLetterListDTO1.setLetter('자');
-
-        BingoCheckByIndexDTO bingoCheckByIndexDTO = new BingoCheckByIndexDTO();
-        bingoCheckByIndexDTO.setMemberId(1L);
-        bingoCheckByIndexDTO.setIndex(7);
-        bingoCheckByIndexDTO.setIsSuccess(true);
-
-        BingoCheckByLetterListDTO bingoCheckByLetterListDTO2 = new BingoCheckByLetterListDTO();
-        bingoCheckByLetterListDTO2.setMemberId(1L);
-        bingoCheckByLetterListDTO2.setLetter('바');
 
         //when
-        bingoService.checkBingoByLetterList(bingoCheckByLetterListDTO1);
-        bingoService.checkBingoByIndex(bingoCheckByIndexDTO);
-        bingoService.checkBingoByLetterList(bingoCheckByLetterListDTO2);
 
         //then
-        Member member = memberService.findOne(1L);
-        assertEquals(member.getBingoBoardId(), 2L);
-
-        BingoStatusResponseDTO bingoStatus = bingoService.getBingoStatus(1L);
-        assertEquals(bingoStatus.getRequiredBingoCount(), 2);
-        assertEquals(bingoStatus.getBingoCount(), 0);
     }
 }
